f21a0a335d34be20c4bf1742bb22cbc9cf10ecee
MrOutsider <16367984+MrOutsider@users.noreply.github.com>
2024-01-07 16:52:53 -0800

    Animations Updated

--- Node2D.gd
+++ Node2D.gd
@@ -9,8 +9,8 @@ extends CharacterBody2D
 enum PLAYER_STATES {IDLE, MOVING, ATTACKING, DASHING, HURTING}
 var player_state : int = PLAYER_STATES.IDLE
 
-enum PLAYER_POINTING {UP, DOWN, LEFT, RIGHT}
-var player_pointing : int = PLAYER_POINTING.DOWN
+enum PLAYER_DIRECTION_STATES {UP, DOWN, LEFT, RIGHT}
+var player_direction_state : int = PLAYER_DIRECTION_STATES.DOWN
 
 var input_dir : Vector2 = Vector2.ZERO
 
@@ -27,8 +27,17 @@ func _input(event):
 		input_dir = input_dir.normalized()
 	if (input_dir.length() < 0.1):
 		input_dir = Vector2.ZERO
+	
+	match player_state:
+		PLAYER_STATES.IDLE:
+			if (input_dir.length() > 0.0):
+				player_state = PLAYER_STATES.MOVING
+		PLAYER_STATES.MOVING:
+			if (input_dir.length() == 0.0):
+				player_state = PLAYER_STATES.IDLE
 
 func _process(delta):
+	player_dir_to_state()
 	animate_sprite()
 
 func _physics_process(delta):
@@ -38,15 +47,50 @@ func _physics_process(delta):
 		velocity = lerp(velocity, Vector2.ZERO, friction)
 	move_and_slide()
 
+func player_dir_to_state() -> void:
+	match player_state:
+		PLAYER_STATES.IDLE:
+			if (abs(input_dir.y) > abs(input_dir.x)):
+				if (input_dir.y > 0.0):
+					player_direction_state = PLAYER_DIRECTION_STATES.DOWN
+				else:
+					player_direction_state = PLAYER_DIRECTION_STATES.UP
+			elif (abs(input_dir.y) < abs(input_dir.x)):
+				if (input_dir.x > 0.0):
+					player_direction_state = PLAYER_DIRECTION_STATES.RIGHT
+				else:
+					player_direction_state = PLAYER_DIRECTION_STATES.LEFT
+		PLAYER_STATES.MOVING:
+			if (abs(input_dir.y) > abs(input_dir.x)):
+				if (input_dir.y > 0.0):
+					player_direction_state = PLAYER_DIRECTION_STATES.DOWN
+				else:
+					player_direction_state = PLAYER_DIRECTION_STATES.UP
+			elif (abs(input_dir.y) < abs(input_dir.x)):
+				if (input_dir.x > 0.0):
+					player_direction_state = PLAYER_DIRECTION_STATES.RIGHT
+				else:
+					player_direction_state = PLAYER_DIRECTION_STATES.LEFT
+
 func animate_sprite() -> void:
 	match player_state:
 		PLAYER_STATES.IDLE:
-			match player_pointing:
-				PLAYER_POINTING.UP:
+			match player_direction_state:
+				PLAYER_DIRECTION_STATES.UP:
 					anim_player.play("idle_up")
-				PLAYER_POINTING.DOWN:
+				PLAYER_DIRECTION_STATES.DOWN:
 					anim_player.play("idle_down")
-				PLAYER_POINTING.LEFT:
+				PLAYER_DIRECTION_STATES.LEFT:
 					anim_player.play("idle_left")
-				PLAYER_POINTING.RIGHT:
+				PLAYER_DIRECTION_STATES.RIGHT:
 					anim_player.play("idle_right")
+		PLAYER_STATES.MOVING:
+			match player_direction_state:
+				PLAYER_DIRECTION_STATES.UP:
+					anim_player.play("move_up")
+				PLAYER_DIRECTION_STATES.DOWN:
+					anim_player.play("move_down")
+				PLAYER_DIRECTION_STATES.LEFT:
+					anim_player.play("move_left")
+				PLAYER_DIRECTION_STATES.RIGHT:
+					anim_player.play("move_right")
