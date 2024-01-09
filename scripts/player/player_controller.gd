55dd053bd1d60641ad31df3588dd506976c59091
MrOutsider <16367984+MrOutsider@users.noreply.github.com>
2024-01-08 16:59:26 -0800

    State Machine Attack
    Attack is now ready to animate. The code to rigger and reset state is ready.

--- scripts/player/player_controller.gd
+++ scripts/player/player_controller.gd
@@ -14,7 +14,7 @@ var player_state : int = PLAYER_STATES.IDLE
 enum PLAYER_DIRECTION_STATES {UP, DOWN, LEFT, RIGHT}
 var player_direction_state : int = PLAYER_DIRECTION_STATES.DOWN
 
-var state_change_anim : bool = true
+var animation_change : bool = true
 
 var input_dir : Vector2 = Vector2.ZERO
 
@@ -24,6 +24,13 @@ func _ready():
 func _input(event):
 	if (Input.is_action_pressed("ui_cancel")):
 		get_tree().quit()
+	
+	if (Input.is_action_just_pressed("attack")):
+		player_state = PLAYER_STATES.ATTACKING
+		animation_change = true
+	
+	if (Input.is_action_just_pressed("ui_left")):
+		reset_after_attack()
 	
 	if (player_state == PLAYER_STATES.IDLE || player_state == PLAYER_STATES.MOVING):
 		input_dir.x = Input.get_action_raw_strength("move_right") - Input.get_action_raw_strength("move_left")
@@ -37,24 +44,25 @@ func _input(event):
 		PLAYER_STATES.IDLE:
 			if (input_dir.length() > 0.0):
 				player_state = PLAYER_STATES.MOVING
-				state_change_anim = true
+				animation_change = true
 		PLAYER_STATES.MOVING:
 			if (input_dir.length() == 0.0):
 				player_state = PLAYER_STATES.IDLE
-				state_change_anim = true
+				animation_change = true
 
 func _process(delta):
 	player_dir_to_state()
-	attack_phase()
 	animate_sprite()
 
 func _physics_process(delta):
-	if (input_dir.length() > 0.0):
+	# Accelerate Or Decelerate
+	if (input_dir.length() > 0.0 && player_state == PLAYER_STATES.MOVING):
 		velocity = lerp(velocity, input_dir * speed * 20.0, acceleration)
 	else:
 		velocity = lerp(velocity, Vector2.ZERO, friction)
+		
+	# Move via velocity but also interact with collisions
 	if (move_and_slide()):
-		# Check all collisions and apply force.
 		for i in get_slide_collision_count():
 			var col = get_slide_collision(i)
 			if (col.get_collider() is RigidBody2D):
@@ -62,45 +70,41 @@ func _physics_process(delta):
 					col.get_collider().apply_force((col.get_normal() * pushing_force)* -1.0)
 					velocity = col.get_collider().linear_velocity
 
-func attack_phase() -> void:
-	if (Input.is_action_just_pressed("attack")):
-		pass
-
 func player_dir_to_state() -> void:
 	match player_state:
 		PLAYER_STATES.IDLE:
 			if (abs(input_dir.y) > abs(input_dir.x)):
 				if (input_dir.y > 0.0):
 					player_direction_state = PLAYER_DIRECTION_STATES.DOWN
-					state_change_anim = true
+					animation_change = true
 				else:
 					player_direction_state = PLAYER_DIRECTION_STATES.UP
-					state_change_anim = true
+					animation_change = true
 			elif (abs(input_dir.y) < abs(input_dir.x)):
 				if (input_dir.x > 0.0):
 					player_direction_state = PLAYER_DIRECTION_STATES.RIGHT
-					state_change_anim = true
+					animation_change = true
 				else:
 					player_direction_state = PLAYER_DIRECTION_STATES.LEFT
-					state_change_anim = true
+					animation_change = true
 		PLAYER_STATES.MOVING:
 			if (abs(input_dir.y) > abs(input_dir.x)):
 				if (input_dir.y > 0.0):
 					player_direction_state = PLAYER_DIRECTION_STATES.DOWN
-					state_change_anim = true
+					animation_change = true
 				else:
 					player_direction_state = PLAYER_DIRECTION_STATES.UP
-					state_change_anim = true
+					animation_change = true
 			elif (abs(input_dir.y) < abs(input_dir.x)):
 				if (input_dir.x > 0.0):
 					player_direction_state = PLAYER_DIRECTION_STATES.RIGHT
-					state_change_anim = true
+					animation_change = true
 				else:
 					player_direction_state = PLAYER_DIRECTION_STATES.LEFT
-					state_change_anim = true
+					animation_change = true
 
 func animate_sprite() -> void:
-	if (state_change_anim):
+	if (animation_change):
 		match player_state:
 			PLAYER_STATES.IDLE:
 				match player_direction_state:
@@ -122,4 +126,18 @@ func animate_sprite() -> void:
 						anim_player.play("move_left")
 					PLAYER_DIRECTION_STATES.RIGHT:
 						anim_player.play("move_right")
-		state_change_anim = false
+			PLAYER_STATES.ATTACKING:
+				match player_direction_state:
+					PLAYER_DIRECTION_STATES.UP:
+						anim_player.play("idle_up") # Attack
+					PLAYER_DIRECTION_STATES.DOWN:
+						anim_player.play("idle_down") # Attack
+					PLAYER_DIRECTION_STATES.LEFT:
+						anim_player.play("idle_left") # Attack
+					PLAYER_DIRECTION_STATES.RIGHT:
+						anim_player.play("idle_right") # Attack
+		animation_change = false
+
+func reset_after_attack() -> void:
+	player_state = PLAYER_STATES.IDLE
+	animation_change = true
