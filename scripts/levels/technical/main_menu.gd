193757310d36c6f50a25fd730d83a2ad7ff37784
MrOutsider <16367984+MrOutsider@users.noreply.github.com>
2024-01-10 14:54:33 -0800

    Physics Update To Top Of Doors

--- scripts/levels/technical/main_menu.gd
+++ scripts/levels/technical/main_menu.gd
@@ -1,16 +1,23 @@
 extends Node
 
+@onready var WORLD = $World
+@onready var main_menu = $MainMenu
+
 @export var new_game_button : Button
 @export var load_game_button : Button
 @export var options_button : Button
 @export var quit_button : Button
+
+@onready var test_level_1 : PackedScene = preload("res://levels/technical/test_level_1.tscn")
 
 func _ready():
 	new_game_button.pressed.connect(start_new_game)
 	quit_button.pressed.connect(quit_game)
 
 func start_new_game() -> void:
-	pass
+	main_menu.queue_free()
+	var n = test_level_1.instantiate()
+	WORLD.add_child(n)
 
 func quit_game() -> void:
 	get_tree().quit()
