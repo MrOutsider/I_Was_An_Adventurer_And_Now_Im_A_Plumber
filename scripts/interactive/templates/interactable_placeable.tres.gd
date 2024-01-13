74c68d988ac10da87ecff55e6b8190af55041a72
MrOutsider <16367984+MrOutsider@users.noreply.github.com>
2024-01-12 23:30:19 -0800

    Update interactable_placeable.tres.gd

--- scripts/interactive/templates/interactable_placeable.tres.gd
+++ scripts/interactive/templates/interactable_placeable.tres.gd
@@ -3,8 +3,8 @@ extends Node2D
 @export var pre_place_sprite : Sprite2D
 @export var placed_sprite : Sprite2D
 @export var focus_sprite : Sprite2D
-@onready var area_2d : CollisionObject2D = $Interactable
-@onready var static_body_2d : CollisionObject2D = $PlacedBody
+@onready var area_2d : Area2D = $Interactable
+@onready var static_body_2d : StaticBody2D = $PlacedBody
 
 @export var can_use : bool = false
 @export var placed : bool = false
@@ -19,13 +19,11 @@ func _ready():
 # Interactable Code
 func focus() -> void:
 	if (!placed):
-		pre_place_sprite.hide()
 		focus_sprite.show()
 
 func lose_focus() -> void:
 	if (!placed):
 		focus_sprite.hide()
-		pre_place_sprite.show()
 
 # Placeable Code
 func use() -> void:
@@ -35,4 +33,6 @@ func use() -> void:
 		pre_place_sprite.hide()
 		placed_sprite.show()
 		static_body_2d.set_collision_layer_value(1, true)
+		area_2d.monitorable = false
+		area_2d.call_deferred("queue_free")
 		switch_used.emit(true)
